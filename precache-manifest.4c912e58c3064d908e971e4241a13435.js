d3058595a8e0118ca07051aae7441504d568fa13
whliang2 <whliang2@illinois.edu>
2020-08-27 11:52:06 -0500

    Updates

---
+++ precache-manifest.4c912e58c3064d908e971e4241a13435.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8d13d34d92be99273db3645a32e481",
+    "url": "/robot_friends_react/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robot_friends_react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "11c4ba08b2d2af5d14f2",
+    "url": "/robot_friends_react/static/css/main.735e836c.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robot_friends_react/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot_friends_react/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c4ba08b2d2af5d14f2",
+    "url": "/robot_friends_react/static/js/main.73dc3459.chunk.js"
+  },
+  {
+    "revision": "4e09c7439b121b2768d7",
+    "url": "/robot_friends_react/static/js/runtime-main.bfe30635.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot_friends_react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
